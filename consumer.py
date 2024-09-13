9bf40c5d7a01bd1e18be6d19b6cb6bd079036fbe
Abner <abcdmartins40@gmail.com>
2024-09-13 15:57:16 -0300

    fist commit

---
+++ consumer.py
@@ -0,0 +1,8 @@
+import pika
+import json
+from typing import Dict
+
+class RabbitPublisher:
+
+    rabbit_publisher = RabbitPublisher()
+    rabbit_publisher = send_message ({"Message":"Inscreva-se no canal"})
